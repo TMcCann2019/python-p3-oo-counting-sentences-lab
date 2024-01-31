f12603e6bc635c365f321c87667819b315de3432
Tim McCann <timothysmccann@gmail.com>
2024-01-31 10:32:02 -0500

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
+  def __init__(self, value=""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    if self.value.endswith('.'):
+      return True
+    else:
+      return False
+
+  def is_question(self):
+    if self.value.endswith('?'):
+      return True
+    else:
+      return False
+
+  def is_exclamation(self):
+    if self.value.endswith("!"):
+      return True
+    else:
+      return False
+
+  def count_sentences(self):
+    sentences = re.split(r'[.!?]', self.value)
+    non_empty_sentences = filter(None, sentences)
+    return len(list(non_empty_sentences))
   pass
